ec595f2754c64a0117269e4c5a8e742ef5e6532e
Kevien Fidel <kevademfide@gmail.com>
2020-08-03 14:24:23 +0800

    enabled nodemon to watch any changes made and rerun the server

--- index.js
+++ index.js
@@ -4,35 +4,45 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-const { User } = require('./model/user');
+const {
+  User
+} = require('./model/user');
 
 mongoose
-	.connect('mongodb+srv://KevFidel:rmpylk62nq@react-blog.yuxgh.mongodb.net/test?retryWrites=true&w=majority', {
-		useNewUrlParser: true,
-		useUnifiedTopology: true // added due to deprecation message
-	})
-	.then(() => console.log('DB connected'))
-	.catch((error) => console.error(error));
+  .connect('mongodb+srv://KevFidel:rmpylk62nq@react-blog.yuxgh.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true // added due to deprecation message
+  })
+  .then(() => console.log('DB connected'))
+  .catch((error) => console.error(error));
 
 app.use(
-	bodyParser.urlencoded({
-		extended: true
-	})
+  bodyParser.urlencoded({
+    extended: true
+  })
 );
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+  res.json("hello world")
+})
+
+
 app.post('/api/users/register', (req, res) => {
-	const user = new User(req.body);
+  const user = new User(req.body);
 
-	user.save((err, data) => {
-		if (err) return res.json({ success: false, err });
+  user.save((err, data) => {
+    if (err) return res.json({
+      success: false,
+      err
+    });
 
-		return res.status(200).json({
-			success: true,
-			data
-		});
-	});
+    return res.status(200).json({
+      success: true,
+      data
+    });
+  });
 });
 
 app.listen(5000);
