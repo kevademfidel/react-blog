cd47e1d04440177caf0b70613d1d05ed31818c95
Kevien Fidel <kevademfide@gmail.com>
2020-08-03 13:53:04 +0800

    added post method that will enable saving data to mongodb

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-mongoose.connect('mongodb+srv://KevFidel:rmpylk62nq@react-blog.yuxgh.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('DB connected'))
-  .catch(error => console.error(error))
+const { User } = require('./model/user');
 
+mongoose
+	.connect('mongodb+srv://KevFidel:rmpylk62nq@react-blog.yuxgh.mongodb.net/test?retryWrites=true&w=majority', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true // added due to deprecation message
+	})
+	.then(() => console.log('DB connected'))
+	.catch((error) => console.error(error));
 
-app.get('/', (req, res) => (
-  res.send('hello world')
-));
+app.use(
+	bodyParser.urlencoded({
+		extended: true
+	})
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.listen(5000)
+app.post('/api/users/register', (req, res) => {
+	const user = new User(req.body);
+
+	user.save((err, data) => {
+		if (err) return res.json({ success: false, err });
+
+		return res.status(200).json({
+			success: true,
+			data
+		});
+	});
+});
+
+app.listen(5000);
