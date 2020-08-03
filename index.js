0038573b5040f23eda671e0644a85a973b02ecf4
Kevien Fidel <kevademfide@gmail.com>
2020-08-03 15:41:17 +0800

    add: hash password with bcrypt

--- index.js
+++ index.js
@@ -7,40 +7,40 @@ const config = require('./config/key');
 const { User } = require('./model/user');
 
 mongoose
-	.connect(config.mongoURI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true // added due to deprecation message
-	})
-	.then(() => console.log('DB connected'))
-	.catch((error) => console.error(error));
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true // added due to deprecation message
+  })
+  .then(() => console.log('DB connected'))
+  .catch((error) => console.error(error));
 
 app.use(
-	bodyParser.urlencoded({
-		extended: true
-	})
+  bodyParser.urlencoded({
+    extended: true
+  })
 );
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-	res.json('hello world');
+  res.json('hello world');
 });
 
 app.post('/api/users/register', (req, res) => {
-	const user = new User(req.body);
+  const user = new User(req.body);
 
-	user.save((err, data) => {
-		if (err)
-			return res.json({
-				success: false,
-				err
-			});
+  user.save((err, data) => {
+    if (err)
+      return res.json({
+        success: false,
+        err
+      });
 
-		return res.status(200).json({
-			success: true,
-			data
-		});
-	});
+    return res.status(200).json({
+      success: true,
+      userData: data
+    });
+  });
 });
 
 app.listen(5000);
--- model/user.js
+++ model/user.js
@@ -1,37 +1,58 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    maxlength: 50,
+    maxlength: 50
   },
   email: {
     type: String,
     trim: true,
-    unique: 1,
+    unique: 1
   },
   password: {
     type: String,
-    minlength: 5,
+    minlength: 5
   },
   lastname: {
     type: String,
-    maxlength: 50,
+    maxlength: 50
   },
   role: {
     type: Number,
-    default: 0,
+    default: 0
   },
   token: {
-    type: String,
+    type: String
   },
   tokenExp: {
-    type: Number,
-  },
-})
+    type: Number
+  }
+});
 
-const User = mongoose.model('User', userSchema)
+userSchema.pre('save', function(next) {
+  let user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = {
   User
-}
+};
